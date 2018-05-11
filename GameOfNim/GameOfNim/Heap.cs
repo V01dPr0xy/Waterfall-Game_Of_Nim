b5407fbe622c8b9a26f69b6bbf0d2093ab4480b2
Zac Snowbank <ZSnowbank@student.neumont.edu>
2018-05-11 11:41:46 -0600

    Programmed Heap

---
+++ GameOfNim/GameOfNim/Heap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNim.Models
+{
+    public class Heap
+    {
+        public int Amount { get; private set; }
+        public bool IsEmpty { get { return Amount == 0; } }
+
+        public string name;
+
+        public bool TakeAmount(int amountToTake)
+        {
+            if (amountToTake <= 0 || amountToTake > Amount) return false;
+
+            Amount -= amountToTake;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return name + " : " + Amount;
+        }
+    }
+}
