36ca18e656d40e398e36a86a16181b2970b82069
Zac Snowbank <ZSnowbank@student.neumont.edu>
2018-05-12 15:33:08 -0600

    Base Player Completed and Documented

--- GameOfNim/GameOfNim/BasePlayer.cs
+++ GameOfNim/GameOfNim/BasePlayer.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace GameOfNim.Models
 {
-    class BasePlayer
+    abstract class BasePlayer
     {
+        public string name;
+
+        /// <summary>
+        /// Takes turn in Nim. Takes an amount great than zero 
+        /// but less than the amount of one of the heaps passed 
+        /// from that heap
+        /// </summary>
+        /// <param name="heaps"> the heaps that an amount can be taken from </param>
+        public abstract void TakeTurn(Dictionary<string, Heap> heaps);
     }
 }
